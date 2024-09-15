8148d51aff2939f70ba71941825baad4b84ce454
AngelicaLeo <151537251+AngelicaLeo@users.noreply.github.com>
2024-09-15 18:35:58 +0200

    Create mappa.js

---
+++ mappa.js
@@ -0,0 +1,6 @@
+// mappa.js
+
+function initMap() {
+    var location = { lat: 45.4642, lng: 9.1900 }; //  coordinate reali
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15
